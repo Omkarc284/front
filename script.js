97a4c399fb0ed8aff6879bd96a66f999006cfd58
Shreshta Arava <shreshta@Shreshtas-MacBook-Air.local>
2023-12-19 17:54:09 +0530

    sm images

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 document.getElementById("appointment-form").addEventListener("submit", function(event) {
     event.preventDefault();
+    var patientname = document.getElementById("patientname").value;
+    var email = document.getElementById("email").value;
+    var clinic = document.getElementById("clinic").value;
+    var phone = document.getElementById("phone").value;
+    var date = document.getElementById("date").value;
+    var time = document.getElementById("time").value;
+    var obj = {
+        patientname:patientname,
+        email:email,
+        clinic:clinic,
+        phone:phone,
+        date:date,
+        time:time
+    }
+    console.log(
+        "DATA: ",obj
+    )
     alert("Appointment booked successfully!");
 });
+function othername() {
+    var input = document.getElementById("userInput").value;
+    alert(input);
+}
