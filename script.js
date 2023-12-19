e4ee429d00caef3fae2a253d34be5b4e3cd044c2
Pranjali Dubey <pranjali.dubey@cartesian.com>
2023-12-20 02:13:10 +0530

    Api Test

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+var enabled_dates = [];
+async function checkenabled() {
+    const response = await fetch("http://68.183.89.222:3120/slots/enabled_dates",{
+        method: "GET",
+        mode: "*cors", // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: "follow", // manual, *follow, error
+        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    })
+    console.log(response.json())
+    return response.json(); // parses JSON response into native JavaScript objects
+}
+enabled_dates = checkenabled()
 document.getElementById("appointment-form").addEventListener("submit", function(event) {
     event.preventDefault();
     var patientname = document.getElementById("patientname").value;
@@ -23,3 +41,4 @@ function othername() {
     var input = document.getElementById("userInput").value;
     alert(input);
 }
+
