68aea1bb2979c5600a3c34603a887f5d7694ff9c
Omkar Chauhan <omkarc284@gmail.com>
2023-12-21 01:20:42 +0530

    final draft1

--- script.js
+++ script.js
@@ -1,19 +1,97 @@
 var enabled_dates = [];
+var data = []
 async function checkenabled() {
-    const response = await fetch("http://68.183.89.222:3120/slots/enabled_dates",{
+    const url =`http://68.183.89.222:3120/slots/enabled_data`
+    await fetch(url,{
         method: "GET",
-        // include, *same-origin, omit
         headers: {
-        "Content-Type": "application/json",
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
- // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            "Content-Type": "text/plain"
+        }
+    }).then((response) => {
+        response.json().then(result => {
+            //console.log("obj:",result.enabled)
+            enabled_dates = result.enabled
+            data = result.data
+            console.log(result)
+            document.getElementById('date').flatpickr({
+                enable: enabled_dates
+            })
+            document.getElementById('time')
+        })
+        
     })
-    console.log(response.json())
-    return response.json(); // parses JSON response into native JavaScript objects
+ // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    
+    // parses JSON response into native JavaScript objects
 }
-enabled_dates = checkenabled()
-document.getElementById("appointment-form").addEventListener("submit", function(event) {
+checkenabled()
+
+document.getElementById('date').addEventListener('input', async function(event){
+    event.preventDefault();
+    var clinic = document.getElementById("clinic").value;
+    var date = document.getElementById("date").value;
+    var dte = new Date(date).toISOString()
+    
+    console.log(dte)
+    const slot  = data.filter(s => (s.branch == clinic && s.date == dte))
+    console.log('Slot: ',slot)
+    if(slot.length ==0){
+        alert("This clinic has no slots for the day. Change either the clinic or the date")
+        document.getElementById('date').value = ''
+    }
+    document.getElementById("time").removeAttribute('disabled')
+    var dynamicSelect = document.getElementById("time");
+    var length = dynamicSelect.options.length;
+    for (i = length-1; i >= 0; i--) {
+        dynamicSelect.options[i] = null;
+    }
+    if(slot[0].slots.length == 0){
+        alert("This clinic has no slots for the day. Change either the clinic or the date")
+        document.getElementById('date').value = ''
+    }
+    slot[0].slots.forEach(t => {
+        var newOption = document.createElement("option");
+        newOption.text = t;
+        newOption.value = t;
+        dynamicSelect.add(newOption)
+    })
+
+})
+document.getElementById('clinic').addEventListener('change', async function(event){
+    event.preventDefault();
+    var clinic = document.getElementById("clinic").value;
+    if(document.getElementById("date").value != '' || undefined || null) {
+        var date = document.getElementById("date").value;
+        var dte = new Date(date).toISOString()
+        
+        console.log(dte)
+        const slot  = data.filter(s => (s.branch == clinic && s.date == dte))
+        console.log('Slot: ',slot)
+        if(slot.length ==0){
+            alert("This clinic has no slots for the day. Change either the clinic or the date")
+            document.getElementById('date').value = ''
+        }
+        var dynamicSelect = document.getElementById("time");
+        var length = dynamicSelect.options.length;
+        for (i = length-1; i >= 0; i--) {
+            dynamicSelect.options[i] = null;
+        }
+        if(slot[0].slots.length == 0){
+            alert("This clinic has no slots for the day. Change either the clinic or the date")
+            document.getElementById('date').value = ''
+        }
+        slot[0].slots.forEach(t => {
+            var newOption = document.createElement("option");
+            newOption.text = t;
+            newOption.value = t;
+            dynamicSelect.add(newOption)
+        })
+
+    }
+    
+})
+
+document.getElementById("appointment-form").addEventListener("submit", async function(event) {
     event.preventDefault();
     var patientname = document.getElementById("patientname").value;
     var email = document.getElementById("email").value;
@@ -22,17 +100,33 @@ document.getElementById("appointment-form").addEventListener("submit", function(
     var date = document.getElementById("date").value;
     var time = document.getElementById("time").value;
     var obj = {
-        patientname:patientname,
-        email:email,
-        clinic:clinic,
-        phone:phone,
-        date:date,
-        time:time
+        'name':patientname,
+        'email':email,
+        'branch':clinic,
+        'phoneNumber':phone,
+        'date':date,
+        'timeSlot':time
     }
-    console.log(
-        "DATA: ",obj
-    )
-    alert("Appointment booked successfully!");
+    const url =`http://68.183.89.222:3120/appointments/new`
+    await fetch(url,{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(obj)
+    }).then((response) => {
+        response.json().then(result => {
+            console.log("obj:",result)
+            alert("Appointment booked successfully!\nCheck your email inbox for confirmation at ",email);
+            window.location.reload()
+        })
+        
+    }).catch(err =>{
+        alert("Something went wrong! \n Error: \n ", err);
+        window.location.reload();
+    })
+    console.log(obj)
+    
 });
 function othername() {
     var input = document.getElementById("userInput").value;
